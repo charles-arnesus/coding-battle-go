447fee9f7d4f16789ace1026409a01159d97f1b8
charles arnesus <charlesarnesus.matulessy@mitrais.com>
2025-09-11 13:39:56 +0700

    add booking model

--- models/booking/bookingSystemModel.go
+++ models/booking/bookingSystemModel.go
@@ -1,8 +1,24 @@
 package booking_model
 
-import "gorm.io/gorm"
+import (
+	flight_model "github.com/charles-arnesus/coding-battle-go/models/flight"
+	"gorm.io/gorm"
+)
 
 type BookingSystem struct {
 	gorm.Model
 	IsActive bool `gorm:"not null"`
+}
+
+type Booking struct {
+	gorm.Model
+	Code string `gorm:"uniqueIndex; not null"`
+}
+
+type BookingFlightRoute struct {
+	gorm.Model
+	BookingID     uint
+	FlightRouteID uint
+	Booking       Booking                  `gorm:"foreignKey:BookingID;"`
+	FlightRoute   flight_model.FlightRoute `gorm:"foreignKey:FlightRouteID;"`
 }
--- models/flight/flightRouteModel.go
+++ models/flight/flightRouteModel.go
@@ -1,6 +1,7 @@
 package flight_model
 
 import (
+	user_model "github.com/charles-arnesus/coding-battle-go/models/user"
 	"gorm.io/gorm"
 )
 
@@ -13,4 +14,14 @@ type FlightRoute struct {
 	DepartureCity     Destination `gorm:"foreignKey:DepartureCityID;"`
 	DestinationCity   Destination `gorm:"foreignKey:DestinationCityID;"`
 	ScheduledDay      int         `gorm:"not null"`
+	Status            string      `gorm:"not null"`
+}
+
+type FlightRouteSeats struct {
+	gorm.Model
+	FlightRouteID uint
+	UserID        uint
+	SeatNumber    int
+	FlightRoute   FlightRoute     `gorm:"foreignKey:FlightRouteID;"`
+	User          user_model.User `gorm:"foreignKey:UserID;"`
 }
